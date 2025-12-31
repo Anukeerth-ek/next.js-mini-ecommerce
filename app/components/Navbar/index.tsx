65df24fe577b36ee3fa48c5c7a01044aa995c2d2
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2025-12-31 22:24:51 +0530

    feat: added navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,8 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex item-center w-full bg-[#191919] py-2 px-14 justify-between">
+      <Image src="/nike-nav-img.png" alt="nike-nav-img" width={52} height={52} />
+      <div className='flex items-center'>
+        <span className='font-bold text-md'>Logout</span>
+      </div>
+    </div>
   )
 }
 
