8768d349df156d517169706674da7eb8d4dc875f
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2026-01-01 08:19:49 +0530

    feat: added the footer page

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { FaFacebookF, FaXTwitter } from "react-icons/fa6";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+     return (
+          <footer className="w-full bg-black py-16 px-25">
+               <div className="max-w-7xl mx-auto flex items-center justify-between">
+                    <div className="flex items-center">
+                         <Image src="/nike-nav-img.png" alt="nike-img" width={106} height={56} />
+                    </div>
 
-export default Footer
+                    <div className="flex items-center gap-4">
+                         <button className="w-21 h-22 rounded-full transition-colors flex items-center justify-center">
+                              <FaFacebookF />
+                         </button>
+
+                         <button className="w-21 h-22 rounded-full transition-colors flex items-center justify-center">
+                              <AiFillInstagram />
+                         </button>
+
+                         <button className="w-21 h-22 rounded-full transition-colors flex items-center justify-center">
+                              <FaXTwitter />
+                         </button>
+                    </div>
+               </div>
+          </footer>
+     );
+};
+
+export default Footer;
