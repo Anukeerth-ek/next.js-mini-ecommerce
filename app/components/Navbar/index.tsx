f3e676fa348e476ce47bc71e9b64bfba1d924325
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2026-01-01 12:34:55 +0530

    Fix: fixed the navigation bug

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -5,12 +5,16 @@ import { FaUserCircle } from "react-icons/fa";
 const Navbar = () => {
      return (
           <div className="flex item-center w-full bg-[#191919] py-2 px-14 justify-between">
-               <Image src="/nike-nav-img.png" alt="nike-nav-img" width={52} height={52} />
+               <Link href="/">
+                    <>
+                         <Image src="/nike-nav-img.png" alt="nike-nav-img" width={52} height={52} />
+                    </>
+               </Link>
                <div className="flex items-center gap-4 text-white">
                     <Link href="/my-orders" aria-label="View orders">
                          <FaUserCircle className="cursor-pointer text-2xl hover:opacity-80" />
                     </Link>
-                    <Link href="/Login">
+                    <Link href="/profile">
                          <span className="font-bold text-md">
                               <button className="cursor-pointer">Logout</button>
                          </span>
